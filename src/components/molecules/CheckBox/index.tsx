2631f6e304c63b6d2340570026f76bc8a9928a18
澤田悠太 <yuta2929ayax@gmail.com>
2022-10-20 15:07:04 +0900

    fix error for using key

--- src/components/molecules/CheckBox/index.tsx
+++ src/components/molecules/CheckBox/index.tsx
@@ -3,14 +3,13 @@ import Label from "@/components/atoms/Label";
 import styles from "@/components/molecules/CheckBox/styles.module.scss";
 
 type Props = {
-  key: Number;
   name: string;
   index: Number;
 };
 
 const CheckBox = (props: Props) => {
   return (
-    <div className={styles.checkbox} key={props.key}>
+    <div className={styles.checkbox}>
       <Input index={props.index} name={props.name} />
       <Label index={props.index} name={props.name} />
     </div>
