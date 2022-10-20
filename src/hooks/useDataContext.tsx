5445e90da65f5472444695e52a5e32ce76425d92
澤田悠太 <yuta2929ayax@gmail.com>
2022-10-20 17:44:00 +0900

    edit useDataContext to use common type

--- src/hooks/useDataContext.tsx
+++ src/hooks/useDataContext.tsx
@@ -1,21 +1,51 @@
 import { createContext, useState, useContext } from "react";
 
-const dataContext = createContext();
+export type dataContextType = {
+  dataList: {
+    name: string;
+    data: {
+      year: number;
+      value: number;
+    }[];
+  }[];
+  setDataList: React.Dispatch<
+    React.SetStateAction<
+      {
+        name: string;
+        data: {
+          year: number;
+          value: number;
+        }[];
+      }[]
+    >
+  >;
+};
+
+export type dataListType = {
+  name: string;
+  data: {
+    year: number;
+    value: number;
+  }[];
+}[];
+
+export type dataValueType = {
+  name: string;
+  data: {
+    year: number;
+    value: number;
+  }[];
+};
+
+// デフォルトの初期値を入れることを忘れずに
+const dataContext = createContext({} as dataContextType);
 
 export function useDataContext() {
   return useContext(dataContext);
 }
 
-export const CountProvider = ({ children }) => {
-  const [dataList, setDataList] = useState<
-    {
-      name: string;
-      data: {
-        year: number;
-        value: number;
-      }[];
-    }[]
-  >([]);
+export const CountProvider = ({ children }: any) => {
+  const [dataList, setDataList] = useState<dataListType>([]);
 
   const value = {
     dataList,
