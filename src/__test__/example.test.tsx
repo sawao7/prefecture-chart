b01a4fb9198e5347a20eb394500cc08d6ff112c9
澤田悠太 <yuta2929ayax@gmail.com>
2022-10-21 14:07:07 +0900

    set test environment

--- src/__test__/example.test.tsx
+++ src/__test__/example.test.tsx
@@ -1,9 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Label from "@/components/atoms/Label";
+import MainTitle from "@/components/atoms/MainTitle";
+import Chart from "@/containers/atoms/Chart";
 
-describe("大タイトル", () => {
-  test("大タイトルが表示される", () => {
-    render(<Label name="北海道" index={0} />);
-    // expect(screen.getByText("大タイトル")).toBeInTheDocument();
+describe("Atoms", () => {
+  test("Label", () => {
+    render(<Label index={1} name="テスト" />);
+  });
+  // test("Chart", () => {
+  //   render(<Chart />);
+  // });
+  test("MainTitle", () => {
+    render(<MainTitle />);
   });
 });
