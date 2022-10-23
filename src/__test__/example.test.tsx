e1064e2640eaffcbd6654e8ef968a6acdf25565a
澤田悠太 <yuta2929ayax@gmail.com>
2022-10-24 01:09:52 +0900

    wrote test code but some error code has error

--- src/__test__/example.test.tsx
+++ src/__test__/example.test.tsx
@@ -1,4 +1,6 @@
 import { render, screen } from "@testing-library/react";
+import ChartPresenter from "@/components/atoms/Chart";
+import InputPresenter from "@/components/atoms/Input";
 import Label from "@/components/atoms/Label";
 import MainTitle from "@/components/atoms/MainTitle";
 import Title from "@/components/atoms/Title";
@@ -7,6 +9,9 @@ import CheckBoxes from "@/components/organisms/CheckBoxes";
 import Header from "@/components/organisms/Header";
 import Main from "@/components/organisms/Main";
 import Chart from "@/containers/atoms/Chart";
+import Input from "@/containers/atoms/Input";
+
+import Home from "@/pages";
 
 describe("Atoms", () => {
   test("Label", () => {
@@ -15,8 +20,22 @@ describe("Atoms", () => {
   // test("Chart", () => {
   //   render(<Chart />);
   // });
-  // test("Input", () => {
-  //   render(<Input />);
+  test("ChartPresenter", () => {
+    const options = {
+      type: "line",
+      name: "北海道",
+      data: [
+        [10, 1990],
+        [20, 2000],
+      ],
+    };
+    render(<ChartPresenter options={options} />);
+  });
+  // test("InputPresenter", () => {
+  //   const index = 1;
+  //   const name = "北海道";
+  //   const getPeopleData =
+  //   render(<InputPresenter />);
   // });
   test("MainTitle", () => {
     render(<MainTitle />);
@@ -26,24 +45,31 @@ describe("Atoms", () => {
   });
 });
 
-describe("Molecules", () => {
-  test("CheckBox", () => {
-    const name = "テスト";
-    const index = 1;
-    render(<CheckBox name={name} index={index} />);
-  });
-});
+// describe("Molecules", () => {
+//   test("CheckBox", () => {
+//     const name = "テスト";
+//     const index = 1;
+//     render(<CheckBox name={name} index={index} />);
+//   });
+// });
 
 describe("Organisms", () => {
-  test("CheckBoxes", () => {
-    const prefectures = ["北海道", "東京都"];
-    render(<CheckBoxes />);
-  });
+  // test("CheckBoxes", () => {
+  //   const prefectures = ["北海道", "東京都"];
+  //   render(<CheckBoxes prefectures={prefectures}/>);
+  // });
   test("Header", () => {
     render(<Header />);
   });
-  test("Main", () => {
+  // test("Main", () => {
+  //   const prefectures = ["北海道", "東京都"];
+  //   render(<Main prefectures={prefectures} />);
+  // });
+});
+
+describe("Pages", () => {
+  test("index", () => {
     const prefectures = ["北海道", "東京都"];
-    render(<Main prefectures={prefectures} />);
+    render(<Home prefectures={prefectures} />);
   });
 });
